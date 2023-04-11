4102f4bac59a85c20d0c4178593bfd7e54a5e2ae
khuramspartan <ksaddique@terafort.com>
2023-04-11 12:30:03 +0500

    ksngroos number line jump

--- Assets/NumberLineJumps/Script/NumberLineJumps.cs
+++ Assets/NumberLineJumps/Script/NumberLineJumps.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//speed = distance/jumps
+//distance= speed*jumps
+//after "j" jumps/seconds both kangros reach at same position
+//distace = v1*j;
+//total distance = (v1*j)+x1(starting position)
+// both kangroos position must be equal if return "True"
+// x1+(v1*j)=x2+(v2*j)
+//x2-x1= (v1-v2)*j
+// j= (x2-x1)/v1-v2) after j jump they meet at point
+//if v1 is greater than v2 and (x2 - x1) % (v1 - v2) == 0 they they will meet otherwise not 
 public class NumberLineJumps : MonoBehaviour
 {
     [SerializeField] private int x1, v1, x2, v2;
@@ -12,10 +22,8 @@ public class NumberLineJumps : MonoBehaviour
 
     public string Kangaroo(int x1, int v1, int x2, int v2)
     {
-        if (v1 < v2 && v1 < v2)
-            return "NO";
 
-        if (v1 != v2 && (x2 - x1) % (v1 - v2) == 0) return "YES";
+        if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0) return "YES";
         else
             return "NO";
 
